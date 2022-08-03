46e73752fe73004c543b70f718d16b89a3c4fa0a
matthewfoxmi <matthewfoxmi@gmail.com>
2022-08-03 16:27:21 -0400

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'menu';
+  title = 'Menu';
+  
+menu:MenuItem[] = 
+[
+  {Item: "Chicken Fingers", Category: "Dinner", Price: 11.99},
+  {Item: "Pizza", Category: "Dinner", Price: 11.99},
+  {Item: "Wings", Category: "Sides", Price: 8.99},
+  {Item: "Breadsticks", Category: "Sides", Price: 4.99},
+  {Item: "Ceasar Salad", Category: "Salads", Price: 5.99},
+  {Item: "Cinnamon Roll", Category: "Dessert", Price: 8.99}
+];
 }
---
+++ src/app/menu-item.ts
@@ -0,0 +1,6 @@
+export interface MenuItem 
+{
+    Item: string;
+    Category: string;
+    Price: number;
+}
